ab1cb601b96e13f96987fe8fee93dc63a6424175
LuckiPhoenix <huynhchihen2005@gmail.com>
2025-10-02 21:44:19 +0700

    feat(auth): enhance authentication module with user registration and login functionality - Added AuthController methods for user registration, login, password reset, and email verification - Updated AuthService to handle user registration and login logic with Prisma - Introduced global response interceptor and exception filter for consistent API responses

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,11 +6,17 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { AllExceptionFilter } from './common/filters/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionFilter());
+  
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
