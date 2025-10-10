9e6606e3c2a9d3cab171e3ec803955adc63538c4
LuckiPhoenix <huynhchihen2005@gmail.com>
2025-10-10 15:07:12 +0700

    feat(validation): enhance validation in registration DTO and global exception handling - Added ValidationPipe to the main application for global request validation - Updated RegisterRequestDto to include password, phone number, and Identity Number validation rules - Enhanced AllExceptionFilter to handle validation errors and provide detailed error messages

--- server/src/auth/dto/register.dto.ts
+++ server/src/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, MinLength, IsOptional, IsEnum } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsOptional, IsEnum, Matches } from 'class-validator';
 
 enum UserType {
   INDIVIDUAL = 'individual',
@@ -11,10 +11,16 @@ export class RegisterRequestDto {
 
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, {
+    message: 'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+  })
   password: string;
 
   @IsOptional()
   @IsString()
+  @Matches(/^(0)\d{9}$/, {
+    message: 'Phone number must be a valid Vietnamese phone number',
+  })
   phone_number?: string;
 
   @IsOptional()
@@ -23,6 +29,10 @@ export class RegisterRequestDto {
 
   @IsOptional()
   @IsString()
+  @Matches(/^(001|002|004|006|008|010|011|012|014|015|017|019|020|022|024|025|026|027|030|031|033|034|035|036|037|038|040|042|044|045|046|048|049|051|052|054|056|058|060|062|064|066|067|068|070|072|074|075|077|079|080|082|083|084|086|087|089|091|092|093|094|095|096)[0-3]\d{2}\d{6}$/,
+  {
+    message: 'Identity number must be a valid Vietnamese Citizen Identity Card',
+  })
   identity_number?: string;
 
   @IsOptional()
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
@@ -13,6 +13,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
   
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new AllExceptionFilter());
